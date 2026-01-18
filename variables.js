4a5db1b8dcf59edbe54906a40df3615a475686df
Aditikalwaghe <aditikalwaghe@gmail.com>
2026-01-18 18:05:26 +0530

    First JavaScript project commit

---
+++ variables.js
@@ -0,0 +1,49 @@
+// var variable
+var name = "Aditi";
+console.log(name); 
+
+name = "Sakshi";
+console.log(name); 
+
+console.log(age); 
+var age = 22;
+console.log(age); 
+ 
+if(true)
+{
+    var y=30;
+}
+console.log(y);
+
+//let Variable
+let name1 = "Aditi";
+console.log(name1); 
+
+ name1 = "Sakshi";
+ console.log(name1); 
+
+//console.log(age1); 
+let age1 = 22;
+console.log(age1); 
+ 
+if(true)
+{
+    let y=30;
+    console.log(y);
+}
+// console.log(y);
+
+
+// const variable
+const PI=3.14;
+    console.log(PI);
+
+//PI=3.1415;
+
+if(true)
+{
+    let gravity=9.8;
+    console.log(gravity);
+}
+//   console.log(gravity);
+
